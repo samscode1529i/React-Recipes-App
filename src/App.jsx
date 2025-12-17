a75601c86713660bf929c7040ec4aeeb1c527d2a
Samuel Irenikase <samscode1529i@gmail.com>
2025-12-17 19:21:35 +0100

    done with the recepie grid nd cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from './components/Navbar/Navbar'
 import Home from './pages/Home'
 import Blog from './pages/blog'
@@ -8,19 +8,15 @@ import Contact from './pages/Contact'
 
 const App = () => {
   return (
-    <div>
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path ="/" element={<Home/>} />
-          <Route path ="/blog" element={<Blog/>} />
-          <Route path ="/recipie" element={<Recipie/>} />
-          <Route path ="/contact" element={<Contact/>} />
-          
-        </Routes>
-      </Router>
-      
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/recipie" element={<Recipie />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   )
 }
 
